dd48041bb7727e9a17e26553bb92c26575844f70
edhannah124-dotcom <edhannah124@gmail.com>
2025-08-31 11:56:06 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ if (!OPENAI_API_KEY) {
 
 const client = new OpenAI({ apiKey: OPENAI_API_KEY });
 const app = express();
+const sem = { acquire: async () => {}, release: () => {} };
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
